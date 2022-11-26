e7185e60f6cd108b8ed2e36b0974badd9c688cde
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-26 17:39:16 +0600

    Dinamic Product card has been added

---
+++ src/Pages/ProductDetailsCard/ProductDetailsCard.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const ProductDetailsCard = ({ product }) => {
+  const {
+    product_name,
+    author,
+    image_url,
+    details,
+    selling_price,
+    buying_price,
+    duration_use,
+    location,
+    is_Verified,
+  } = product;
+
+  console.log(product);
+
+  return (
+    <div className="max-w-lg p-4 shadow-md ">
+      <div className="flex justify-between pb-4 border-bottom">
+        <div className="flex items-center">
+          <p className="mb-0 capitalize dark:text-black">
+            Name: {author?.name}
+          </p>
+        </div>
+        <p>Email: {author?.email}</p>
+      </div>
+      <div className="space-y-4">
+        <div className="space-y-2">
+          <img
+            src={image_url}
+            alt=""
+            className="block object-cover object-center w-full rounded-md h-72 dark:bg-gray-500"
+          />
+          <div className="flex items-center text-xs">
+            <span>{author?.published_date} ago</span>
+          </div>
+          <div>
+            <p className="text-xl font-bold">{product_name}</p>
+            <p className="text-lg">Selling Price: ${selling_price}</p>
+            <p className="text-lg">Buying Price: ${buying_price}</p>
+            <small className="text-sm">Duration of Use: {duration_use}</small>
+          </div>
+        </div>
+        <div className="space-y-2">
+          <h3 className="text-xl font-semibold dark:text-violet-400">
+            <span className="font-bold">Details About Phone:</span> {details}
+          </h3>
+          <p className="leading-snug dark:text-black">
+            <span className="font-semibold">
+              Meting Location or Your location:
+            </span>{" "}
+            {location}
+          </p>
+        </div>
+      </div>
+      <div className="mt-4 card-actions justify-end">
+        <button className="btn btn-sm btn-primary">Book Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetailsCard;
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import ProductDetailsCard from "../ProductDetailsCard/ProductDetailsCard";
 
 const Products = () => {
   const productsCategory = useLoaderData();
-  console.log(productsCategory);
   return (
     <div>
-      <h2>hi</h2>
+      <h2 className="text-3xl font-semibold">
+        This section has {productsCategory.length} products
+      </h2>
+      <div className="mt-6 grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {productsCategory.map((product) => (
+          <ProductDetailsCard
+            key={product._id}
+            product={product}
+          ></ProductDetailsCard>
+        ))}
+      </div>
     </div>
   );
 };
