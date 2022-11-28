32384ce7b9c19f1f53f66fa082000ccf8ba50cc0
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-28 19:16:01 +0600

    Seller verify button has been attached with verify sellet API and when we click it it's worked

--- src/Pages/Dashboard/Sellers/Sellers.jsx
+++ src/Pages/Dashboard/Sellers/Sellers.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 const Sellers = () => {
   const { data: users = [], refetch } = useQuery({
@@ -11,6 +12,26 @@ const Sellers = () => {
       return data;
     },
   });
+
+  const handleVerify = (email) => {
+    fetch(`http://localhost:5000/users/${email}`, {
+      method: "PUT",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Seller Successfully Verified",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          refetch();
+        }
+      });
+  };
+
   return (
     <div>
       <h2 className="text-3xl font-semibold mb-4">All Sellers</h2>
@@ -21,6 +42,7 @@ const Sellers = () => {
               <th></th>
               <th>Name</th>
               <th>Email</th>
+              <th>Verify Seller</th>
               <th>Delete</th>
             </tr>
           </thead>
@@ -30,6 +52,16 @@ const Sellers = () => {
                 <th>{i + 1}</th>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
+                <td>
+                  {user?.verify !== true && (
+                    <button
+                      onClick={() => handleVerify(user.email)}
+                      className="btn btn-xs btn-primary"
+                    >
+                      Verify
+                    </button>
+                  )}
+                </td>
                 <td>
                   <button className="btn btn-xs btn-secondary">Delete</button>
                 </td>
