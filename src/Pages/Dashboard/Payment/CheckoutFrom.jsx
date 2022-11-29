6a1515b41366f813ec4976696b4fb59f8f6f5874
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-29 22:42:03 +0600

    Pyment methor has been impliment with from and connect with stripe and confirm and error messgae has been added

---
+++ src/Pages/Dashboard/Payment/CheckoutFrom.jsx
@@ -0,0 +1,118 @@
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import React, { useEffect, useState } from "react";
+
+const CheckoutFrom = ({ booking }) => {
+  const [cardError, setCardError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const [transactionId, setTransactionId] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+
+  const stripe = useStripe();
+  const elements = useElements();
+  const { sellingPrice, productName, email } = booking;
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:5000/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify({ sellingPrice }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, [sellingPrice]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log(error);
+      setCardError(error.message);
+    } else {
+      setCardError("");
+    }
+    setSuccess("");
+    setProcessing(true);
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: productName,
+            email: email,
+          },
+        },
+      });
+
+    if (confirmError) {
+      setCardError(confirmError.message);
+      return;
+    }
+    if (paymentIntent.status === "succeeded") {
+      setSuccess("Congrats! Your Payment Completed");
+      setTransactionId(paymentIntent.id);
+      //store payment info in the database
+    }
+    setProcessing(false);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
+              },
+            },
+          }}
+        />
+        <button
+          className="btn btn-sm mt-4 btn-primary"
+          type="submit"
+          disabled={!stripe || !clientSecret || processing}
+        >
+          Pay
+        </button>
+      </form>
+      <p className="text-red-500">{cardError}</p>
+      {success && (
+        <div>
+          <p className="text-green-500">{success}</p>
+          <p>
+            Your transactionId:{" "}
+            <span className="font-bold">{transactionId}</span>
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CheckoutFrom;
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,17 +1,29 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import CheckoutFrom from "./CheckoutFrom";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+console.log(stripePromise);
 
 const Payment = () => {
   const booking = useLoaderData();
-  console.log(booking);
+
   return (
     <div>
       <h2 className="text-3xl font-semibold">
         Payment for {booking.productName}
       </h2>
       <p className="text-xl">
-        Please pay {booking.sellingPrice} for your purchase product.
+        Please pay <strong>${booking.sellingPrice}</strong> for your purchase
+        product.
       </p>
+      <div className="w-96 my-6">
+        <Elements stripe={stripePromise}>
+          <CheckoutFrom booking={booking} />
+        </Elements>
+      </div>
     </div>
   );
 };
