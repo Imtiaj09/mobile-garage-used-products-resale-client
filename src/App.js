9e5563931ca92395cef2735464b9d180aa3cfcb3
Imtiaj09 <ahmimtiaj@gmail.com>
2022-11-29 17:18:53 +0600

    Home Page Loading has been added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import HomePageLoading from './Pages/Shared/HomePageLoading/HomePageLoading';
 import router from './Routes/Routes/Routes';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false)
+    }, 1500);
+  }, [])
+
   return (
     <div className='max-w-[1440px] mx-auto'>
-      <RouterProvider router={router}></RouterProvider>
+      {
+        loading ? <HomePageLoading /> : <>
+          <RouterProvider router={router}></RouterProvider>
+        </>
+      }
     </div>
   );
 }
